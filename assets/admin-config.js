e095cad6a2166d6e0eeda2b426acf39dbb0765da
JGWebStyle <2808joel@gmail.com>
2026-05-25 00:35:43 -0400

    Add files via upload

---
+++ assets/admin-config.js
@@ -0,0 +1,4 @@
+window.ANGELS_ADMIN_CONFIG = {
+  "apiBaseUrl": "https://script.google.com/macros/s/AKfycbw7l_IyUV_R70TfyIzOqUC8hk0qySep2apa-zo6Zp5dv2F0LZO7hmSnQcMaCrro11XMzg/exec",
+  "publicUrl": "https://script.google.com/macros/s/AKfycbw7l_IyUV_R70TfyIzOqUC8hk0qySep2apa-zo6Zp5dv2F0LZO7hmSnQcMaCrro11XMzg/exec"
+};
---
+++ assets/site-data.js
@@ -0,0 +1,5 @@
+window.ANGELS_SITE_DATA = {
+  "dataUrl": "data/site-data.json",
+  "apiBaseUrl": "https://script.google.com/macros/s/AKfycbw7l_IyUV_R70TfyIzOqUC8hk0qySep2apa-zo6Zp5dv2F0LZO7hmSnQcMaCrro11XMzg/exec",
+  "mode": "api"
+};
