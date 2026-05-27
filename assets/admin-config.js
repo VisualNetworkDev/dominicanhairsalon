470f70ee898771320451060e00eb493a58b57ae7
JGWebStyle <2808joel@gmail.com>
2026-05-27 13:49:21 -0400

    Update admin-config.js

--- assets/admin-config.js
+++ assets/admin-config.js
@@ -4,6 +4,6 @@ window.ANGELS_ADMIN_CONFIG = {
   "publicApiUrl": "https://script.google.com/macros/s/AKfycbyQBO8JDgFThgEGhkK9-4jW0jsaMP2u4TbHuya5FyUreKrB4CBkV2MA0lfWFlx6aVMTLw/exec",
   "developerApiUrl": "https://script.google.com/macros/s/AKfycbx4xPvQJG1mhylxqTZ5Kl6YcYCaU2mohbAoYJco8tDWLg2qDnoFsxZZQzRCuSKoUTD7/exec",
   "developerDashboardUrl": "https://visualnetworkdev.github.io/developer/index.html",
-  "publicUrl": "https://visualnetworkdev.github.io/dominicanhairsalon/index.html",
+  "publicUrl": "https://visualnetworkdev.github.io/dominicanhairsalon/",
   "qrScannerUrl": "https://visualnetworkdev.github.io/dominicanhairsalon/qr-scanner.html"
 };
