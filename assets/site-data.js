09bd249e088a78ab69255845f4ae768745f19bd5
JGWebStyle <2808joel@gmail.com>
2026-05-27 12:54:33 -0400

    Add files via upload

---
+++ assets/site-data.js
@@ -0,0 +1,6 @@
+window.ANGELS_SITE_DATA = {
+  "dataUrl": "/dominicanhairsalon/data/site-data.json",
+  "apiBaseUrl": "https://script.google.com/macros/s/AKfycbyQBO8JDgFThgEGhkK9-4jW0jsaMP2u4TbHuya5FyUreKrB4CBkV2MA0lfWFlx6aVMTLw/exec",
+  "publicApiUrl": "https://script.google.com/macros/s/AKfycbyQBO8JDgFThgEGhkK9-4jW0jsaMP2u4TbHuya5FyUreKrB4CBkV2MA0lfWFlx6aVMTLw/exec",
+  "mode": "api"
+};
