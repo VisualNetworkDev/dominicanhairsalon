b379b8b059e27152801ee5371ee45895c027de1c
JGWebStyle <2808joel@gmail.com>
2026-05-27 11:46:11 -0400

    Update admin-config.js

--- assets/admin-config.js
+++ assets/admin-config.js
@@ -1,8 +1,9 @@
 window.ANGELS_ADMIN_CONFIG = {
   "apiBaseUrl": "https://script.google.com/macros/s/AKfycbwDAoJA_Hf8AIOtymBCJO3mlrkx7OWXeQIlhHVNlunaX162cnJtj6ZhBKJw8Di5XXq7-w/exec",
   "adminApiUrl": "https://script.google.com/macros/s/AKfycbwDAoJA_Hf8AIOtymBCJO3mlrkx7OWXeQIlhHVNlunaX162cnJtj6ZhBKJw8Di5XXq7-w/exec",
+  "publicApiUrl": "https://script.google.com/macros/s/AKfycbyQBO8JDgFThgEGhkK9-4jW0jsaMP2u4TbHuya5FyUreKrB4CBkV2MA0lfWFlx6aVMTLw/exec",
   "developerApiUrl": "https://script.google.com/macros/s/AKfycbx4xPvQJG1mhylxqTZ5Kl6YcYCaU2mohbAoYJco8tDWLg2qDnoFsxZZQzRCuSKoUTD7/exec",
-  "developerDashboardUrl": "https://script.google.com/macros/s/AKfycbx4xPvQJG1mhylxqTZ5Kl6YcYCaU2mohbAoYJco8tDWLg2qDnoFsxZZQzRCuSKoUTD7/exec",
+  "developerDashboardUrl": "https://visualnetworkdev.github.io/developer/index.html",
   "publicUrl": "https://visualnetworkdev.github.io/dominicanhairsalon/index.html",
-  "qrScannerUrl": "qr-scanner.html"
+  "qrScannerUrl": "https://visualnetworkdev.github.io/dominicanhairsalon/qr-scanner.html"
 };
