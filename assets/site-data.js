ed228ce1560de8b4ed34e6ad06052fec2df93b5b
JGWebStyle <2808joel@gmail.com>
2026-05-29 18:33:49 -0400

    Update site-data.js

--- assets/site-data.js
+++ assets/site-data.js
@@ -1,6 +1,6 @@
 window.ANGELS_SITE_DATA = {
   "dataUrl": "/dominicanhairsalon/data/site-data.json",
-  "apiBaseUrl": "https://script.google.com/macros/s/AKfycbyQBO8JDgFThgEGhkK9-4jW0jsaMP2u4TbHuya5FyUreKrB4CBkV2MA0lfWFlx6aVMTLw/exec",
+  "apiBaseUrl": "https://script.google.com/macros/s/AKfycbyYMymwom__yETjqFgYZWyRCxe5md7YVG6m2uPLwwHrh4zGmFnehl9XHmiQFJkuzVqS_Q/exec",
   "publicApiUrl": "https://script.google.com/macros/s/AKfycbyQBO8JDgFThgEGhkK9-4jW0jsaMP2u4TbHuya5FyUreKrB4CBkV2MA0lfWFlx6aVMTLw/exec",
   "mode": "api"
 };
