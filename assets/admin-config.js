773e260f3616c68886f8877d39063e72f734362e
JGWebStyle <2808joel@gmail.com>
2026-05-29 19:02:12 -0400

    Update admin-config.js

--- assets/admin-config.js
+++ assets/admin-config.js
@@ -1,7 +1,7 @@
 window.ANGELS_ADMIN_CONFIG = {
   "apiBaseUrl": "https://script.google.com/macros/s/AKfycbzJ2S8OEOaosU6nqBk6PHhMdYOavjnFqqVxvLi7DJTVtYh1A4HSj1N31PWwv5LLQLDXwQ/exec",
   "adminApiUrl": "https://script.google.com/macros/s/AKfycbzJ2S8OEOaosU6nqBk6PHhMdYOavjnFqqVxvLi7DJTVtYh1A4HSj1N31PWwv5LLQLDXwQ/exec",
-  "publicApiUrl": "https://script.google.com/macros/s/AKfycbyQBO8JDgFThgEGhkK9-4jW0jsaMP2u4TbHuya5FyUreKrB4CBkV2MA0lfWFlx6aVMTLw/exec",
+  "publicApiUrl": "https://script.google.com/macros/s/AKfycbwFGn869QEqkSHH-iS99Nx5mJqANgFCFv9qUtjyF0SdaM2tfKq3PGL32TsWyYJnWUIy/exec",
   "developerApiUrl": "https://script.google.com/macros/s/AKfycbx4xPvQJG1mhylxqTZ5Kl6YcYCaU2mohbAoYJco8tDWLg2qDnoFsxZZQzRCuSKoUTD7/exec",
   "developerDashboardUrl": "https://visualnetworkdev.github.io/developer/index.html",
   "publicUrl": "https://visualnetworkdev.github.io/dominicanhairsalon/",
