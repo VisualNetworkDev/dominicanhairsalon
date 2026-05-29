dfb71e1e8121c796373f3f7e2202edaf6769d960
JGWebStyle <2808joel@gmail.com>
2026-05-29 18:33:29 -0400

    Update client-actions-config.js

--- assets/client-actions-config.js
+++ assets/client-actions-config.js
@@ -1,3 +1,3 @@
 window.DHS_CLIENT_ACTIONS_CONFIG = {
-  apiBaseUrl: 'https://script.google.com/macros/s/AKfycbxPSrD4zDg43ZW09IYUfYmHt5Zhqz1pe_vrLIEhLBdUhkxrjlmyCY-2G1xtfaXhLl_u/exec'
+  apiBaseUrl: 'https://script.google.com/macros/s/AKfycbyYMymwom__yETjqFgYZWyRCxe5md7YVG6m2uPLwwHrh4zGmFnehl9XHmiQFJkuzVqS_Q/exec'
 };
